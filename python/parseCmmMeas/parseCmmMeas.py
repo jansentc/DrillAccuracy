fcd2938227108bb99d6fa200151ac69ea2c57f07
Exomoons <Exomoons@users.noreply.github.com>
2014-12-22 13:50:22 -0800

    put the class outputs into an array, which I then wrote to a file to later try to plot

--- python/parseCmmMeas/parseCmmMeas.py
+++ python/parseCmmMeas/parseCmmMeas.py
@@ -1,19 +1,22 @@
 from numpy import *
+import glob
 
 class PlateMeas(object):
     def __init__(self, pathToFile):
         self.pathToFile = pathToFile
         with open(self.pathToFile) as f:
             self.linesinfile = f.readlines() 
-        #array = [self.plateID(), self.MeasDate(), self.ResidRadErrRMS()]  
-        #print array      
-        #    
+        #array = [self.plateID(), self.MeasDate(), self.ResidRadErrRMS()]  #Before, when I was trying to create the array [self.plateID,...] 
+        #print array                             #I was just giving it the function
+                                                #name, not telling it to DO the function. We fix this by doing self.plateID(), 
+                                                #which will return whatever that function returns.            
+            
     def plateID(self):
         for line in self.linesinfile:
             splitLine = line.split()
             if splitLine[0] == "PlugPlate":
-                    return int(line.split()[1])
-                    #print self.plateID
+                    return int(line.split()[1]) 
+                    
         # another way 
         #f = open(self.pathToFile)
         #for line in f.readlines():
@@ -23,37 +26,42 @@ class PlateMeas(object):
         #f.close()
             
             
-    def MeasDate(self):
+    def measDate(self):
         for line in self.linesinfile:
             splitLine = line.split()
             if splitLine[0] == "MeasDate":
                 return str(line.split()[1])
                 #print self.MeasDate
     
-    def ResidRadErrRMS(self):
+    def residRadErrRMS(self):
         for line in self.linesinfile:
             splitLine = line.split()
             if splitLine[0] == "ResidRadErrRMS":
                 return float(line.split()[2])
                 #print self.ResidRadErrRMS
                 
-    def createarray(self):
-        return [self.plateID(), self.MeasDate(), self.ResidRadErrRMS()]
-                    
+    def createArray(self):
+        return [self.plateID(), self.measDate(), self.residRadErrRMS()] 
                 
     
             
-plate1 = PlateMeas('/Users/tiffanyjansen/Documents/SDSSWork/examplefits.txt')
-plateID = plate1.plateID()
-#MeasDate = plate1.MeasDate()
-#ResidRadErrRMS = plate1.ResidRadErrRMS()
-print plate1.createarray()
+#testplate = PlateMeas('/Users/tiffanyjansen/Documents/SDSSWork/examplefits.txt')
+#test = testplate.createArray()
+#print test
 
-#to create a list of textfiles, use Glob: look up Glob
-#works like: globMeString = "/home/csayres/*.txt)"
-#filenameList = glob.glob(globMeString)
-#
+stringfilenames = '/Users/tiffanyjansen/GitHub/DrillAccuracy/tests/testData/fit*.txt'
+filenameList = glob.glob(stringfilenames)
+#print filenameList
+
+codeOutput = open('codeOutput.txt', 'w')
 #outarray = []
-#for filename in filenameList:
-#    pm = PlateMeas(filename)
-#    outArray.append(pm.createArray)
+for filename in filenameList:
+    pm = PlateMeas(filename).createArray()
+    outarray = array(pm) 
+    #print outarray
+    codeOutput.write(str(outarray) + '\n')
+codeOutput.close()
+
+PlateID, MeasDate, ResidRadErrRMS = loadtxt('codeOutput.txt', dtype = 'string', usecols = (0,1,2), unpack = True)
+
+
