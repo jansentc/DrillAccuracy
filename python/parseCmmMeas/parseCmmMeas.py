3b60a41e044a9358f58253cd44e75b124ef49272
Exomoons <Exomoons@users.noreply.github.com>
2014-12-22 11:35:09 -0800

    Added function to create an array, added notes on arrays

--- python/parseCmmMeas/parseCmmMeas.py
+++ python/parseCmmMeas/parseCmmMeas.py
@@ -1,47 +1,59 @@
 from numpy import *
 
-class DataTable(object):
-    pass
-
 class PlateMeas(object):
     def __init__(self, pathToFile):
         self.pathToFile = pathToFile
         with open(self.pathToFile) as f:
-            self.linesinfile = f.readlines()
-        #array = [self.plateID(), self.MeasDate(), self.ResidRadErrRMS()]
-        #print array
-
+            self.linesinfile = f.readlines() 
+        #array = [self.plateID(), self.MeasDate(), self.ResidRadErrRMS()]  
+        #print array      
+        #    
     def plateID(self):
         for line in self.linesinfile:
             splitLine = line.split()
             if splitLine[0] == "PlugPlate":
-                    self.plateID = int(line.split()[1])
+                    return int(line.split()[1])
                     #print self.plateID
-        # another way
+        # another way 
         #f = open(self.pathToFile)
         #for line in f.readlines():
         #    if "PlugPlate" in line:
         #        self.plateID = int(line.split()[1])
         #        print "second", self.plateID
         #f.close()
-
-
+            
+            
     def MeasDate(self):
         for line in self.linesinfile:
             splitLine = line.split()
             if splitLine[0] == "MeasDate":
-                self.MeasDate = str(line.split()[1])
+                return str(line.split()[1])
                 #print self.MeasDate
-
+    
     def ResidRadErrRMS(self):
         for line in self.linesinfile:
             splitLine = line.split()
             if splitLine[0] == "ResidRadErrRMS":
-                self.ResidRadErrRMS = float(line.split()[2])
+                return float(line.split()[2])
                 #print self.ResidRadErrRMS
+                
+    def createarray(self):
+        return [self.plateID(), self.MeasDate(), self.ResidRadErrRMS()]
+                    
+                
+    
+            
+plate1 = PlateMeas('/Users/tiffanyjansen/Documents/SDSSWork/examplefits.txt')
+plateID = plate1.plateID()
+#MeasDate = plate1.MeasDate()
+#ResidRadErrRMS = plate1.ResidRadErrRMS()
+print plate1.createarray()
 
-
-# plate1 = PlateMeas('/Users/tiffanyjansen/Documents/SDSSWork/examplefits.txt')
-#plate1.plateID()
-#plate1.MeasDate()
-#plate1.ResidRadErrRMS()
+#to create a list of textfiles, use Glob: look up Glob
+#works like: globMeString = "/home/csayres/*.txt)"
+#filenameList = glob.glob(globMeString)
+#
+#outarray = []
+#for filename in filenameList:
+#    pm = PlateMeas(filename)
+#    outArray.append(pm.createArray)
